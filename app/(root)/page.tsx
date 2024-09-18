9627c267c8e3234d4a1e61f0ca02a6cbab983048
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-09-18 23:31:45 +0100

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
+import Promotion from "@/components/Home/Promotion";
 
 export default function Home() {
   return (
-    <main>
-      promotion
+    <main className="lg:max-w-4xl mx-auto xl:max-w-5xl w-full">
+      <Promotion />
       collection
     </main>
   );
--- components/Home/Promotion.tsx
+++ components/Home/Promotion.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Promotion = () => {
   return (
-    <div>
-      Promotion
-    </div>
-  )
-}
+    <section className="py-10">
+      <h1 className="font-bold text-xl md:text-2xl text-center mb-5">
+        Welcome to Store45. See what's new
+      </h1>
+      <p className="opacity-70 max-sm:text-sm text-center font-semibold">
+        Our newest release is deserving of fascination
+      </p>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
 
-export default Promotion
+export default Promotion;
