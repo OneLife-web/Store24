c4502f046dd1269c202c417cd4308265a5a23b37
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-09-19 01:14:18 +0100

    update

--- components/Navigations/Footer.tsx
+++ components/Navigations/Footer.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-primary py-10 max-lg:px-[3%]">
-      <div></div>
+      <div className="lg:max-w-4xl mx-auto xl:max-w-5xl w-full">
+        <h1 className="heading1 text-white text-center">Stay in the know!</h1>
+        <p></p>
+      </div>
     </footer>
   );
 };
