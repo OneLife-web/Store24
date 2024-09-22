7b42f7ccb6086bf6e07bf62bf1ad63f44ea041e1
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-09-22 14:08:06 +0100

    update

--- components/products/SingleProductContainer.tsx
+++ components/products/SingleProductContainer.tsx
@@ -6,8 +6,8 @@ const SingleProductContainer = ({ data }: { data: updateData }) => {
   const { images } = data;
   return (
     <section className="max-sm:px-[3%] py-7">
-      <div className="">
-        <div className="">
+      <div className="flex">
+        <div className="lg:max-w-[60%]">
           <Carousel images={images} />
         </div>
         <div className="">
--- lib/PowerHouse.ts
+++ lib/PowerHouse.ts
@@ -1,6 +1,6 @@
 export const getSettings = async () => {
   try {
-    const res = await fetch("http://localhost:3001/api/settings", {
+    const res = await fetch("https://store24-kohl.vercel.app/api/settings", {
       method: "GET",
       cache: "no-store",
     });
@@ -19,7 +19,7 @@ export const getSettings = async () => {
 
 export const fetchProducts = async () => {
   try {
-    const res = await fetch("http://localhost:3001/api/product", {
+    const res = await fetch("https://store24-kohl.vercel.app/api/product", {
       method: "GET",
       cache: "no-store",
     });
@@ -38,7 +38,7 @@ export const fetchProducts = async () => {
 
 export const fetchProduct = async (id?: string) => {
   try {
-    const res = await fetch(`http://localhost:3001/api/product/${id}`, {
+    const res = await fetch(`https://store24-kohl.vercel.app/api/product/${id}`, {
       method: "GET",
       cache: "no-store",
     });
