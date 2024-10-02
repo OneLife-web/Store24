4da74c1584f39d645b99347e16904c6660fda272
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-10-02 02:32:05 +0100

    faq

---
+++ app/(root)/faq/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FaqPage = () => {
+  return (
+    <div className="px-[3%] pb-20 lg:max-w-4xl min-h-screen mx-auto xl:max-w-5xl w-full">
+      <h1 className="heading1 text-center pt-10 pb-5">FAQ</h1>
+    </div>
+  );
+};
+
+export default FaqPage;
