b503b113ec3eb77f506ff665c5923fba14d6c9d6
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-10-02 01:08:40 +0100

    update

--- components/Home/Collection.tsx
+++ components/Home/Collection.tsx
@@ -3,7 +3,7 @@ import ProductCard from "../Cards/ProductCard";
 
 const Collection = ({ products }: { products: updateData[] }) => {
   return (
-    <section className="py-10 max-lg:px-[3%]">
+    <section className="py-10 pb-40 max-lg:px-[3%]">
       <div>
         <h1 className="heading1">Latest in Store</h1>
         <div className="grid mt-6 grid-cols-2 gap-3 gap-y-10 md:grid-cols-3 lg:grid-cols-4">
