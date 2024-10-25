8c81f4f9e525ed53463a351337faa9432b6accbb
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-10-25 01:08:59 +0100

    update loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,20 +3,20 @@ import Image from "next/image";
 
 const Loader = () => {
   return (
-    <div className="h-screen flex items-center justify-center bg-white">
+    <div className="fixed top-0 bottom-0 right-0 left-0 h-screen flex items-center justify-center bg-black/75">
       <div className="w-fit relative">
         <Loader2
-          size={150}
+          size={32}
           strokeWidth={1.1}
           className="text-secondaryBg animate-spin"
         />
-        <Image
+        {/* <Image
           src="/logo2.png"
           width={100}
           height={100}
           alt="logo"
           className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
-        />
+        /> */}
       </div>
     </div>
   );
